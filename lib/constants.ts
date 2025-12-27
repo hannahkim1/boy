769d02eb086a16c8189a8633962ad955225bbb3c
dkgitcode <danjkim11@gmail.com>
2025-12-27 17:10:13 -0600

    fix scopes

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,13 +3,14 @@ export const SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token";
 export const SPOTIFY_API_BASE = "https://api.spotify.com/v1";
 
 export const SPOTIFY_SCOPES = [
-  "user-read-private",
-  "user-read-email",
-  "user-read-playback-state",
-  "user-read-currently-playing",
-  "user-modify-playback-state",
-  "playlist-modify-private",
-  "playlist-modify-public",
+	"user-read-private",
+	"user-read-email",
+	"user-read-playback-state",
+	"user-read-currently-playing",
+	"user-modify-playback-state",
+	"playlist-modify-private",
+	"playlist-modify-public",
+	"playlist-read-private",
 ].join(" ");
 
 export const TOKEN_STORAGE_KEY = "spotify_tokens";
